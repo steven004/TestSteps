5c1bff000a1adb41c4438c3dae505dedd38c7708
Steven LI <steven004@gmail.com>
2015-01-15 14:20:54 +0800

    fix #7 add passdesc and faildesc to the step and bi-comp functions

--- test_steps/__init__.py
+++ test_steps/__init__.py
@@ -14,11 +14,12 @@ import os, re, time
 from inspect import currentframe
 import operator
 
-__all__ = ['test_logger', 'ok', 'fail', 'eq', 'ne', 'gt','lt', 'le', 'ge', 'match', 'unmatch',
-           'setlogger', 'addBiOperator', 'getOpWrapper', 'step', 'steps', 's']
-
+__all__ = ['test_logger', 'ok', 'fail', 'eq', 'ne', 'gt', 'lt', 'le', 'ge', 'match', 'unmatch',
+           'setlogger', 'addBiOperator', 'getOpWrapper', 'step', 'steps', 's', 'check', 'checks',
+           'addStepOption' ]
 
 __tracebackhide__ = True
+
 
 def __init_logger__():
     global test_logger
@@ -34,12 +35,15 @@ def __init_logger__():
     test_logger.addHandler(fh)
     test_logger.addHandler(ch)
 
+
 test_logger = logging.getLogger("Test")
 test_logger.setLevel(logging.DEBUG)
+
 
 def setlogger(testlogger):
     global test_logger
     test_logger = testlogger
+
 
 class __TestLog__(object):
     def __init__(self):
@@ -64,14 +68,14 @@ class __TestLog__(object):
         self.file_name = co.co_filename
         self.case_no += 1
         self.step_logger.info("------------------------------------------------------")
-        self.step_logger.info("Case %s in file: %s" %(self.case_name, self.file_name))
+        self.step_logger.info("Func %s in file: %s" % (self.case_name, self.file_name))
 
     def new_step(self, pf, step_info, err_msg=''):
         self.step_no += 1
         if pf:
-            self.step_logger.info("Check-%r: %s - PASS: %s" %(self.step_no, step_info, err_msg))
+            self.step_logger.info("Check-%r: %s - PASS - %s" % (self.step_no, step_info, err_msg))
         else:
-            self.step_logger.error("Check-%r: %s - FAIL: %s" %(self.step_no, step_info, err_msg))
+            self.step_logger.error("Check-%r: %s - FAIL - %s" % (self.step_no, step_info, err_msg))
 
 
 _this_file = os.path.normcase(setlogger.__code__.co_filename)
@@ -86,9 +90,10 @@ def _step_closure(func):
     :return: True, or raise exception if error
     """
     __tracebackhide__ = True
+
     def __step__(*args, **kwargs):
         __tracebackhide__ = True
-        #Get current the caller of this function
+        # Get current the caller of this function
         f = currentframe().f_back
         while hasattr(f, "f_code"):
             co = f.f_code
@@ -106,16 +111,18 @@ def _step_closure(func):
         __step_info__.new_step(pf, step_info, err_msg)
         if not pf:
             raise TestStepFail(func.__name__, step_info, err_msg)
-    return(__step__)
+
+    return (__step__)
 
 
 @_step_closure
 def __ok__(cond, desc, errmsg):
     __tracebackhide__ = True
-    return(cond, desc, errmsg)
+    return (cond, desc, errmsg)
+
 
 @_step_closure
-def ok(cond, passdesc = None, faildesc=None):
+def ok(cond, passdesc=None, faildesc=None):
     '''
     :param cond: could be a string, when there is no desc parameter and just pass the step
     :param desc: description of this step
@@ -124,43 +131,47 @@ def ok(cond, passdesc = None, faildesc=None):
     if not passdesc:
         if isinstance(cond, bool):
             passdesc = "ok(%r)" % cond
-        else: # cond is a description actually, pass anyway.
+        else:  # cond is a description actually, pass anyway.
             passdesc = cond
             cond = True
     if not faildesc: faildesc = passdesc
 
     if cond:
-        return(True, passdesc, '')
-    else: return (False, passdesc, faildesc)
+        return (True, passdesc, '')
+    else:
+        return (False, faildesc, '')
 
 
 @_step_closure
-def fail(desc = ''):
-    return(False, "", "fail(%s)"%desc)
+def fail(desc=''):
+    return (False, "", "fail(%s)" % desc)
 
 
 def _bi_comp_closure(op):
     @_step_closure
-    def bi_comp(o1, o2, desc=''):
-        if desc: desc1 = desc
-        else:
-            desc1 = "%r %s %r" %(o1, op, o2)
+    def bi_comp(o1, o2, passdesc='', faildesc=''):
+        if not passdesc:
+            passdesc = "%r %s %r" % (o1, op, o2)
+        if not faildesc:
+            faildesc = "%r %s %r" % (o1, op, o2)
+
         func = _ExtOperation.operator(op)
 
+        err_msg = "%r %s %r?" % (o1, op, o2)
         if func(o1, o2):
-            return(True, desc1, "")
+            return (True, passdesc, "")
         else:
-            err_msg = "%r %s %r?" %(o1, op, o2)
-            return(False, desc, err_msg)
+            return (False, faildesc, err_msg)
 
     return bi_comp
 
-## Define all the normal functions
+# # Define all the normal functions
 eq, ne, gt, lt, ge, le = ( _bi_comp_closure(op) for op in ['==', '!=', '>', '<', '>=', '<='] )
 
 
 class TestStepFail(Exception):
     """ custom exception for error reporting. """
+
 
 class TestRunTimeError(Exception):
     """ custom exception for error reporting. """
@@ -244,6 +255,7 @@ def steps(code_lines, globals=None, locals=None):
             code_string, options = TestStep.parse_steps(ss)
             step(code_string, globals, locals, **options)
 
+
 s = steps
 
 
@@ -268,7 +280,7 @@ class TestStep:
         self.expr1_str, self.expr2_str = (code_string, None)
         self.expr1_val, self.expr2_val = (None, None)
         self.func = None
-        self.func_string = code_string
+        self.pass_str = self.fail_str = code_string
         self.result = False
 
     @classmethod
@@ -278,20 +290,20 @@ class TestStep:
         code_string = step_string
         option_d = {}
         if m:
-            code_string, option_string = m.group(1,2)
+            code_string, option_string = m.group(1, 2)
             short_opt_d = dict((v[0], k) for k, v in TestStepOptions.items())
             ol = re.compile(r'(?<!^)\s+(?=(?:-\w|--\w{2,}))').split(option_string)
             for o in ol:
                 param = None
                 if o[1] != '-':
                     op = o[1]
-                    if not op in short_opt_d: raise Exception("Wrong option %s"%op)
+                    if not op in short_opt_d: raise Exception("Wrong option %s" % op)
                     opt = short_opt_d[op]
                     param = o[2:].strip()
                 else:
                     opt, param = re.compile(r'\s+').split(o[2:], 1)
                     if not opt in TestStepOptions:
-                        raise Exception("Wrong option %s" %op)
+                        raise Exception("Wrong option %s" % op)
                 if TestStepOptions[opt][1] == int:
                     option_d[opt] = int(param)
                 elif TestStepOptions[opt][1] == bool:
@@ -308,9 +320,9 @@ class TestStep:
     def parse(self):
         ## Get op and expr strings if there is an op
         for op in _ExtOperation._operator_dict.keys():
-            m = re.compile(r'(.*)\s(%s)\s(.*)'%op).match(self.code_string)
+            m = re.compile(r'(.*)\s(%s)\s(.*)' % op).match(self.code_string)
             if m:
-                (self.expr1_str, self.op_string, self.expr2_str) = m.group(1,2,3)
+                (self.expr1_str, self.op_string, self.expr2_str) = m.group(1, 2, 3)
                 break
 
         ## Get options
@@ -323,8 +335,8 @@ class TestStep:
                 # elif paramType == 'bool':
                 #     self.options[k][2] = bool(re.compile('T|t|Y|y').match(v))
                 self.options[k][1] = v
-            else: # should not happen
-                raise RuntimeError('ParameterType Error for option: %s' %k)
+            else:  # should not happen
+                raise RuntimeError('ParameterType Error for option: %s' % k)
 
 
     def __exe_string(self):
@@ -332,9 +344,10 @@ class TestStep:
         if self.op_string:
             self.expr2_val = eval(self.expr2_str, self.globals, self.locals)
             ret = _ExtOperation.operator(self.op_string)(self.expr1_val, self.expr2_val)
-            self.err_msg = "%r %s %r" %(self.expr1_val, self.op_string, self.expr2_val)
+            self.err_msg = "%r %s %r" % (self.expr1_val, self.op_string, self.expr2_val)
             #test_logger.debug('--v-- %s --v--'%(self.err_msg))
-        else: self.err_msg = "%r" %(self.expr1_val)
+        else:
+            self.err_msg = "%r" % (self.expr1_val)
 
         self.result = bool(ret)
         return ret
@@ -347,7 +360,8 @@ class TestStep:
                 self.func = TestStepOptions[k][2](self, self.options[k][1])(self.func)
 
         self.result = self.func()
-        __ok__(self.result, self.code_string, self.err_msg)
+        step_description = self.pass_str if self.result else self.fail_str
+        __ok__(self.result, step_description, self.err_msg)
 
     @classmethod
     def _repeat(cls, obj, seconds):
@@ -355,17 +369,17 @@ class TestStep:
             def do_it(*args, **kwargs):
                 p_f = False
                 loop = 0
-                debug_info =''
+                debug_info = ''
                 end_time = time.time() + seconds
-                while(time.time() < end_time):
+                while (time.time() < end_time):
                     p_f = func(*args, **kwargs)
                     loop += 1
-                    debug_info += "%d:<%s>  "%(loop, obj.err_msg)
+                    debug_info += "%d:<%s>  " % (loop, obj.err_msg)
                     if p_f: break
                     time.sleep(1)
-                obj.err_msg += ' - tried %d times in %d seconds'%(loop, seconds)
+                obj.err_msg += ' - tried %d times in %d seconds' % (loop, seconds)
                 obj.result = bool(p_f)
-                test_logger.debug("Results(-r %d set) { %s}"%(seconds, debug_info) )
+                test_logger.debug("Results(-r %d set) { %s}" % (seconds, debug_info))
                 return p_f
 
             return do_it
@@ -378,17 +392,21 @@ class TestStep:
         def _timeout_(func):
             def do_it(*args, **kwargs):
                 import threading
-                t = threading.Thread(target = func, args=args, kwargs=kwargs)
+
+                t = threading.Thread(target=func, args=args, kwargs=kwargs)
                 t.setDaemon(True)
                 t.start()
                 t.join(seconds)
                 if t.is_alive():
-                    obj.err_msg += "  - Step Timeout (-w %d set)" %seconds
+                    obj.err_msg += "  - Step Timeout (-w %d set)" % seconds
                     #test_logger.debug('--v-- step did not complete in %d seconds(-t option set) --v--'%seconds)
                     obj.result = False
                     return False
-                else: return obj.result
+                else:
+                    return obj.result
+
             return do_it
+
         return _timeout_
 
     @classmethod
@@ -400,8 +418,11 @@ class TestStep:
                     obj.result = True
                     #test_logger.debug('--v-- step is not executed (due to -s option set) --v--')
                     return True
-                else: return func(*args, **kwargs)
+                else:
+                    return func(*args, **kwargs)
+
             return __skip__
+
         return _skip_
 
     @classmethod
@@ -412,11 +433,13 @@ class TestStep:
                     ret = func(*args, **kwargs)
                     obj.result = not ret
                     #test_logger.debug('--v-- reverse the result (due to -x option set) --v--')
-                    obj.err_msg += '   - Original result: %r (-x option set) ' %ret
+                    obj.err_msg += '   - Original result: %r (-x option set) ' % ret
                     return obj.result
                 else:
                     return func(*args, **kwargs)
+
             return __xfail__
+
         return _xfail_
 
 
@@ -432,7 +455,9 @@ class TestStep:
                     return obj.result
                 else:
                     return func(*args, **kwargs)
+
             return __warn__
+
         return _warn_
 
     @classmethod
@@ -442,12 +467,14 @@ class TestStep:
                 end_time = time.time() + seconds
                 ret = func(*args, **kwargs)
                 zzz = end_time - time.time()
-                if zzz>0: time.sleep(zzz)
-                obj.err_msg += '  - sleep %d seconds (-d %d set)'% (zzz, seconds)
+                if zzz > 0: time.sleep(zzz)
+                obj.err_msg += '  - sleep %d seconds (-d %d set)' % (zzz, seconds)
                 #test_logger.debug('--v-- sleep %d seconds (due to -d option set) --v--'%zzz)
                 obj.result = ret
                 return ret
+
             return do_it
+
         return _duration_
 
 
@@ -463,6 +490,7 @@ TestStepOptions = {
 
 TestStepOptPriority = ['xfail', 'repeat', 'timeout', 'duration', 'warning', 'skip']
 
+
 def addStepOption(long, short, paraType, func, before=None):
     if before:
         i = TestStepOptPriority.index(before)
@@ -473,6 +501,7 @@ def addStepOption(long, short, paraType, func, before=None):
     TestStepOptions[long] = [short, paraType, func]
 
 
+## Add the exception (-e) option
 def _exception(obj, exception):
     def _exception_(func):
         def do_it(*args, **kwargs):
@@ -480,13 +509,38 @@ def _exception(obj, exception):
                 func(*args, **kwargs)
             except exception:
                 obj.result = True
-                obj.err_msg = ' - exception: %r caught'%exception
+                obj.err_msg = ' - exception: %r caught' % exception
             else:
                 obj.result = False
-                obj.err_msg = ' - exception: %r not caught'%exception
+                obj.err_msg = ' - exception: %r not caught' % exception
             return obj.result
+
         return do_it
+
     return _exception_
 
 addStepOption('exception', 'e', Exception, _exception, 'xfail')
 
+
+## Add passdesc/faildesc (-p/-f) option
+def _pass_desc(obj, pass_str):
+    def _pass_desc_(func):
+        def do_it(*args, **kwargs):
+            obj.pass_str = pass_str
+            return func(*args, **kwargs)
+        return do_it
+    return _pass_desc_
+
+def _fail_desc(obj, fail_str):
+    def _fail_desc_(func):
+        def do_it(*args, **kwargs):
+            obj.fail_str = fail_str
+            return func(*args, **kwargs)
+        return do_it
+    return _fail_desc_
+
+addStepOption('passdesc', 'p', str, _pass_desc, 'xfail')
+addStepOption('faildesc', 'f', str, _fail_desc, 'xfail')
+
+check = step
+checks = steps
