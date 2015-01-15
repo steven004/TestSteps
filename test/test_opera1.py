4070a3b71d535089bc9c12fc61a41a087cf63c81
Steven LI <steven004@gmail.com>
2015-01-15 15:14:14 +0800

    add example about step/steps return

--- test/test_opera1.py
+++ test/test_opera1.py
@@ -3,13 +3,14 @@ __author__ = 'Steven LI'
 
 from test_steps import *
 
+
 def wait():
     import time
     time.sleep(5)
     return True
 
 def test_1():
-    eq(3+5, 8)
+    eq(3+5, 8, "3+5 equals 8")
     ok("ok step")
     ok(3, "3 should pass")
     unmatch("shanghai", "beijing", "to see if shanghai is not beijing")
@@ -37,12 +38,12 @@ def test_2():
     fail("anything")
 
 def test_3step():
-    step("'ok'> '5'")
+    step("'ok'> '5'", passdesc='OK > 5 should pass', faildesc='this should not be printed')
     #step("3 == 5")
     step("wait()", timeout=6, xfail = True, warning=True)
     step("3+6 == 9", duration = 4)
     step("4 == 6", skip = True, duration=10, warning=True)
-    step("abs(4-3) == 1")
+    step("abs(4-3) == 5", faildesc='abs(4-3) not equals 5')
     step("4 == 5", repeat=3, warning=True)
 
 def test_4steps():
@@ -51,15 +52,28 @@ def test_4steps():
         3+6 == 9  -d 4
         3 == 5 -w
         4 == 6  -x
-        abs(4-3) == 1
+        abs(4-3) == 5
         9 > 0/0  -e ZeroDivisionError
         9 < 0/0  -e Exception
         9 == 0/0 -e SyntaxError
         4 == 5  --repeat 3
-        """)
+        """, batch=True)
+
+def test_5steps():
+    ret = steps("""
+        'ok'> '5'
+        3+6 == 9  -d 4
+        3 == 5 -w
+        4 == 6  -x
+        abs(4-3) == 1
+        9 > 0/0  -e ZeroDivisionError
+        9 < 0/0  -e Exception
+        """, batch=True)
+
+    print(ret)
 
 if __name__ == '__main__' :
     #test_1()
-    test_2()
+    #test_2()
     #test_3step()
-    test_4steps()
+    test_5steps()
